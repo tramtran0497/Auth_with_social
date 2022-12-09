034f472bcc582c486605ef02f26517b75cc480db
tramtran0497 <tramtran0497@gmail.com>
2022-12-09 18:39:22 +0200

    install and use react-login-google

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,26 @@
 import Navbar from "./Components/Navbar";
 import './app.css';
 import LogIn from "./Pages/LogIn";
+import { useEffect } from "react";
+import { gapi } from 'gapi-script';
 
 const App = () => {
+    const client_id = process.env.REACT_APP_CLIENT_ID;
+    useEffect(() => {
+        // initialize the client
+        const initClient = () => {
+            gapi.client.init({
+            clientId: client_id,
+            scope: ''
+          });
+       };
+       gapi.load('client:auth2', initClient);
+    });
+
     return (
         <div>
-            <Navbar/>
-            {/* Pages*/}
-            <LogIn/>
+            {/* <Navbar/> Pages*/}
+            <LogIn clientId={client_id}/>
         </div>
     );
 };
--- src/Pages/LogIn.jsx
+++ src/Pages/LogIn.jsx
@@ -1,14 +1,32 @@
 import './login.css';
 import { BsFacebook, BsGoogle, BsGithub } from "react-icons/bs";
+import { GoogleLogin } from 'react-google-login';
 
-const LogIn = () => {
+const LogIn = ({clientId}) => {
+
+  function onSuccess (response) {
+    console.log(response);
+  };
+
+  function onFailure (response) {
+    console.log(response);
+  };
+
   return (
     <div className='wapper'>
         <h3 className='titleLogIn'>LOG IN</h3>
         <div className='logInWapper'>
             <div className='logInLeft'>
                 <div className='socialIcon'><BsFacebook className='icon'/> <span className='textIcon'>Facebook</span></div>
-                <div className='socialIcon'><BsGoogle className='icon'/> <span className='textIcon'>Google</span></div>
+                {/* <div className='socialIcon'><BsGoogle className='icon'/> <span className='textIcon'>Google</span></div>*/}
+                <GoogleLogin
+                  clientId={clientId}
+                  buttonText="Sign in with Google"
+                  onSuccess={onSuccess}
+                  onFailure={onFailure}
+                  cookiePolicy={'single_host_origin'}
+                  isSignedIn={true}
+                />
                 <div className='socialIcon'><BsGithub className='icon'/> <span className='textIcon'>GitHub</span></div>
             </div>
             <div className='logInRight'>
